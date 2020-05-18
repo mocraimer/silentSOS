0352fba470c50fd66e68d7a59b218a0ce5f5ecbb
Mordechai <good2k3@gmail.com>
2020-05-18 17:07:00 +0300

    adding alert button

---
+++ silent_sos/src/components/AlertButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+export default class AlertButton extends React.Component {
+    render() {
+        return (
+            <div>
+                <a href="senderMsg" >
+                    <img class="img-responsive emsbutton" src="https://image.ibb.co/dqibLk/button.png"/>
+                </a>
+            </div>
+        );
+    }
+}
