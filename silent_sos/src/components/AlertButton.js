38bda6919d7a5073988af410b71afd415aa80140
Mordechai <good2k3@gmail.com>
2020-05-19 15:45:26 +0300

    adding localstorage and timestamp for alert

--- silent_sos/src/components/AlertButton.js
+++ silent_sos/src/components/AlertButton.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import {reactLocalStorage} from 'reactjs-localstorage';
 export default class AlertButton extends React.Component {
+    handleAlert = () => { 
+        let timestamp = Date.now();
+        console.log('clicked');
+        if(reactLocalStorage.get('alerts',false,true)){
+            let currentAlerts = reactLocalStorage.get('alerts',undefined,false);
+                currentAlerts = JSON.parse(currentAlerts);
+                currentAlerts.push(timestamp);
+                reactLocalStorage.setObject('alerts', currentAlerts);
+        }
+        else {
+            reactLocalStorage.set('alerts',JSON.stringify([timestamp]))
+        }
+    };
     render() {
         return (
             <div>
-                <a href="sos" >
+                <a onClick={this.handleAlert}>
                     <img class="img-responsive emsbutton" src="https://image.ibb.co/dqibLk/button.png" />
                     <span>Test</span>
                 </a>
